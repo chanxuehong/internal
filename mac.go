c6961cbc2d1cbcc2d148cc336596d4bfb246744b
chan xuehong <chanxuehong@gmail.com>
2015-11-29 10:08:52 +0800

    mac of up state interface is preferred.

--- mac.go
+++ mac.go
@@ -7,7 +7,7 @@ import (
 	"github.com/chanxuehong/rand"
 )
 
-var MAC [6]byte
+var MAC [6]byte // One MAC of this machine; Particular case, it is a random bytes.
 
 func init() {
 	MAC = getMAC()
@@ -21,6 +21,9 @@ func getMAC() (mac [6]byte) {
 		return genMAC()
 	}
 
+	// Gets a MAC from interfaces of this machine,
+	// the MAC of up state interface is preferred.
+	found := false // Says it has found a MAC
 	for _, itf := range interfaces {
 		if itf.Flags&net.FlagLoopback == net.FlagLoopback ||
 			itf.Flags&net.FlagPointToPoint == net.FlagPointToPoint {
@@ -32,10 +35,25 @@ func getMAC() (mac [6]byte) {
 			if bytes.Equal(itf.HardwareAddr, zeroMAC[:6]) {
 				continue
 			}
+			if itf.Flags&net.FlagUp == 0 {
+				if !found {
+					copy(mac[:], itf.HardwareAddr)
+					found = true
+				}
+				continue
+			}
 			copy(mac[:], itf.HardwareAddr)
 			return
 		case 8: // EUI-64
 			if bytes.Equal(itf.HardwareAddr, zeroMAC[:]) {
+				continue
+			}
+			if itf.Flags&net.FlagUp == 0 {
+				if !found {
+					copy(mac[:3], itf.HardwareAddr)
+					copy(mac[3:], itf.HardwareAddr[5:])
+					found = true
+				}
 				continue
 			}
 			copy(mac[:3], itf.HardwareAddr)
@@ -43,10 +61,14 @@ func getMAC() (mac [6]byte) {
 			return
 		}
 	}
+	if found {
+		return
+	}
 
 	return genMAC()
 }
 
+// generates a random MAC.
 func genMAC() (mac [6]byte) {
 	rand.Read(mac[:])
 	mac[0] |= 0x01 // multicast
